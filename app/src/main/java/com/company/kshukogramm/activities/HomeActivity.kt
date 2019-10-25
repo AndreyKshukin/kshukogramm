3756e35768542c52b26a34c43079f9b2d00b5fae
Andrey Kshukin <kshukar@gmail.com>
2019-10-25 22:34:20 +0300

    23 урок. Добавили ленту новостей.

--- app/src/main/java/com/company/kshukogramm/activities/HomeActivity.kt
+++ app/src/main/java/com/company/kshukogramm/activities/HomeActivity.kt
@@ -3,11 +3,18 @@ package com.company.kshukogramm.activities
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.company.kshukogramm.R
 import com.company.kshukogramm.utils.FirebaseHelper
+import com.company.kshukogramm.utils.GlideApp
 import com.company.kshukogramm.utils.ValueEventListenerAdapter
-import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.feed_item.view.*
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
@@ -20,28 +27,51 @@ class HomeActivity : BaseActivity(0) {
         Log.d(TAG, "onCreate")
 
         mFirebase = FirebaseHelper(this)
-
-        sign_out_text.setOnClickListener{
-            mFirebase.auth.signOut()
-        }
         mFirebase.auth.addAuthStateListener {
-            if (it.currentUser == null){
+            if (it.currentUser == null) {
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }
         }
-        mFirebase.database.child("feed-posts").child(mFirebase.auth.currentUser!!.uid)
-            .addValueEventListener(ValueEventListenerAdapter{
-                val posts = it.children.map { it.getValue(FeedPost::class.java)!! }
-                Log.d(TAG,"feedPosts: ${posts.first().timestampDate()}")
-            })
+
     }
 
     override fun onStart() {
         super.onStart()
-        if(mFirebase.auth.currentUser == null) {
+        val currentUser = mFirebase.auth.currentUser
+        if (currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
+        } else {
+            mFirebase.database.child("feed-posts").child(currentUser.uid)
+                .addValueEventListener(ValueEventListenerAdapter {
+                    val posts = it.children.map { it.getValue(FeedPost::class.java)!! }
+                    Log.d(TAG, "feedPosts: ${posts.first().timestampDate()}")
+                    feed_recycle.adapter = FeedAdapter(posts)
+                    feed_recycle.layoutManager = LinearLayoutManager(this)
+                })
         }
+    }
+}
+
+class FeedAdapter(private val posts: List<FeedPost>)
+    : RecyclerView.Adapter<FeedAdapter.ViewHolder>(){
+
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view){}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        val view =LayoutInflater.from(parent.context)
+            .inflate(R.layout.feed_item, parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FeedAdapter.ViewHolder, position: Int){//TODO
+        holder.view.post_image.loadImage(posts[position].image)
+    }
+
+    override fun getItemCount() = posts.size
+
+    private fun ImageView.loadImage(image: String){
+        GlideApp.with(this).load(image).centerCrop().into(this)
     }
 }
